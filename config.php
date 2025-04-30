b5d98f727c006a10f116130ee221ed851b847c84
degerdj <degerdj@gmail.com>
2025-04-30 23:56:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 return array(
 
 	'CallMeDEBUG' => 1, // дебаг сообщения в логе: 1 - пишем, 0 - не пишем
-	'LogPath' => '/var/log/callmeplus-php.log', //По умолчанию /var/log/callmeplus-php.log
+	'LogPath' => '/var/log/callme-php.log', //По умолчанию /var/log/callme-php.log
 	'tech' => 'PJSIP', //Frepbx по умолчанию
 	'authToken' => 'klumlkm2lkmlkh3j', //токен авторизации битрикса для CamllMeOut.php
 	'bitrixApiUrl' => 'https://aaaaaa.bitrix24.ru/rest/7/klupoblkmlkmlk2m34h3j/', //url к api битрикса (входящий вебхук) для отсылки через CallMeIn.php
-	'extentions' => array('1100','s'), // список внешних номеров, через запятую - все равно только с 's' заработало
+	'extentions' => array('1100','s'), // список внешних номеров, через запятую
 	'context' => 'from-internal', //исходящий контекст для оригинации звонка
 	'asterisk' => array( // настройки для подключения к астериску
 		    'host' => '127.0.0.1',
